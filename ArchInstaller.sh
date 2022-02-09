f9fa6c78ecf413ca860eca9324b632da1e7aa922
Stu-Air <99331221+Stu-Air@users.noreply.github.com>
2022-02-09 11:51:55 +0000

    Add files via upload

---
+++ ArchInstaller.sh
@@ -0,0 +1,127 @@
+#! /bin/bash
+
+root_password=""
+user_password=""
+hostname=""
+username=""
+continent_city=""
+chipset=""     # amd or intel
+
+loadkeys uk
+
+echo "Connecting to wifi"
+#iwctl station wlan0 connect Lord\ Voldemodem\ 5G
+#    Ping -c 5 www.google.com
+
+echo "Updating system clock"
+timedatectl set-ntp true
+
+echo "Syncing packages database"
+pacman -Syy --noconfirm
+
+echo "Creating partition tables"
+ (
+ echo d # 	Delete existing Partition 
+ echo   # 	Accept default
+ echo d # 	Delete existing Partition
+ echo   # 	Accept default
+ echo d # 	Delete existing Partition
+ echo   # 	Accept default
+ echo d # 	Delete existing Partition
+ echo   # 	Accept default
+ echo g # 	Create u guid
+ echo n # 	Create new partition 
+ echo   # 	Accept default
+ echo   # 	Accept default
+ echo +512M #   Create first partition size 
+ echo t # 	Select type of partition 
+ echo   # 	Accept default
+ echo 1 # 	Partition type 1 (EFI)
+ echo   # 	Accept default (Partition 1 created)
+ echo n #	Create final partition 
+ echo   #	Accept default
+ echo   #	Accept default
+ echo   #	Accept default (Partition 3 created rest of disk) 
+ echo w #	Write to disk 
+ ) | fdisk /dev/nvme0n1
+
+mkfs.fat -F32 /dev/nvme0n1p1	# Make partition 1 Fat format
+mkfs.ext4 /dev/nvme0n1p2	# Make partition 3 Ext4 Format
+
+echo "Mounting System Partition"
+mount /dev/nvme0n1p2 /mnt
+
+echo "Installing Arch Linux"
+yes '' | pacstrap /mnt base linux-lts linux-firmware linux-lts-headers 
+    
+echo "Generating fstab"
+genfstab -U /mnt >> /mnt/etc/fstab
+
+echo "Configuring new system"
+arch-chroot /mnt /bin/bash <<EOF
+ 
+pacman -S --noconfirm sudo acpi acpid nano wget networkmanager wpa_supplicant wireless_tools netctl dialog grub efibootmgr dosfstools os-prober mtools xorg xorg-apps xf86-video-amdgpu mesa dhcpcd amd-ucode pulseaudio-bluetooth bluez bluez-utils fuse2 unzip gvfs xdg-user-dirs
+xdg-user-dirs-update
+LC_ALL=C xdg-user-dirs-update --force
+
+
+echo "Setting system clock"
+ln -sf /usr/share/zoneinfo/$continent_city /etc/localtime
+hwclock --systohc
+
+echo "Setting locales"
+echo "en_GB.UTF-8 UTF-8" >> /etc/locale.gen
+echo "LANG=en_GB.UTF-8" >> /etc/locale.conf
+echo "KEYMAP=uk" > /etc/vconsole.conf
+locale-gen
+
+echo "Setting hostname"
+echo $hostname > /etc/hostname
+
+echo "Setting root password"
+echo -en "$root_password\n$root_password" | passwd
+
+echo "Creating new user"
+useradd -m -G wheel -s /bin/bash $username
+echo -en "$user_password\n$user_password" | passwd $username
+
+echo "Setting hosts"
+echo "127.0.0.1 	localhost
+::1		localhost
+127.0.1.1. 	"$hostname".localdomain	"$hostname >> /etc/hosts
+
+echo "Generating initramfs"
+mkinitcpio -P
+
+echo "Enabling NetworkManager"
+sudo systemctl enable NetworkManager
+
+echo "Adding user as a sudoer"
+   echo '%wheel ALL=(ALL) ALL' | EDITOR='tee -a' visudo
+
+echo "Installing grub"
+mkdir /boot/efi
+mount /dev/nvme0n1p1 /boot/efi
+grub-install --target=x86_64-efi efi-directory=/boot/efi
+mkdir /boot/grub/locale
+cp /usr/share/locale/en\@quot/LC_MESSAGES/grub.mo /boot/grub/locale/en.mo
+grub-mkconfig -o /boot/grub/grub.cfg
+
+
+echo "Enabling Bluetooth"
+sudo systemctl enable bluetooth
+
+echo "configuring swap"
+sudo dd if=/dev/zero of=/swapfile bs=1024 count=524288
+sudo chown root:root /swapfile
+sudo chmod 0600 /swapfile
+sudo mkswap /swapfile
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+sudo swapon -s
+
+pacman -Syu --noconfirm
+EOF
+
+
+umount -R /mnt
+echo "Arch Linux is ready. You can reboot now!"
---
+++ README.md
@@ -0,0 +1,28 @@
+
+##########
+# Arch Linux Automation Installer Script
+# Author: Stuart Kirker ( Stu )
+# Version: v1.9, 30-08-2020
+# Source: Personal Use
+##########
+
+
+
+## Description
+
+This is a Shell script for the automation of installing my personal Arch linux. This is by no means any complete set of all Settings needed for your system and neither is it another "antispying" type of script. 
+It's simply a installer which I like to use and which in my opinion makes the installation process of Arch Linux on your computer less obtrusive.
+
+THIS SCRIPT INSTALL ESSENTIAL PROGRAMS AND DEVELOPMENT TOOLS
+
+## Usage
+If you just want to run the script with the default preset, download Arch linux and sig file from (https://www.archlinux.org/download/), 
+Verify runing command ( gpg --keyserver-options auto-key-retrieve --verify archlinux-version-x86_64.iso.sig ).
+Make sure you edit the top of the section of the script before booting to your Arch live disk. upon booting into the Arch live disk, 
+you will need to mount the drive/usb you have the script on and simply run sh ./ArchInstaller.sh
+
+
+Please feel free to customize & tweak or even add your own custom tweaks, however these features require some basic knowledge of command line usage and shell scripting.
+
+I AM NOT RESPONSEABLE FOR ANY DAMAGE TO YOUR MACHINE. please read carefully and use you head this will wipe and repartition you drive.
+ 
